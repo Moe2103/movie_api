269b7e142896e3fb2daa35aa0f9b25ea355766de
Moe2103 <95778781+Moe2103@users.noreply.github.com>
2022-03-22 20:49:32 +0100

    Task 2.5

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 let topMovies = [
 
     {
-
+        genre: 'Sci-Fi',
         title: 'Superman vs Batman',
         director: 'Zack Snyder'
 
@@ -17,23 +17,84 @@ let topMovies = [
 
 
      {
+         genre: 'Sci-Fi',
          titel: 'Avengers End Game',
          director: 'Anthony Russo / Joe Russo'
         
      },
 
     {
+        genre: 'Sci-Fi',
         titel: 'Spiderman far from Home',
         director: 'Jon Watts'
+    },
+
+    {
+        genre: 'Sci-Fi',
+        titel: 'Wonder Woman',
+        director: 'Patty Jenkis'
+    },
+    
+    {
+        genre: 'Sci-Fi',
+        titel:'Star Trek',
+        director: 'J. J. Abrams'
+
     }
+
+
+
 ];
 
-
+// return a list of All Movies
 app.get('/movies', (req, res) => {
 res.json(topMovies);
 
 });
 
+
+//return all movies in chosen genre 
+app.get('/movies/:movieTitle', (req, res) => {
+    res.json(topMovies.find((movie) => {
+      return movie.title === req.params.movieTitle }));
+  });
+
+
+  //return all movies in selected GENRE
+  app.get('/movies/genre/:movieGenre', (req, res) => {
+    res.json(topMovies.filter((movie) => {
+      return movie.genre === req.params.movieGenre;
+    }));
+
+});  
+
+//return selected by name DIRECTOR's BIO
+app.get('/directors/:name', (req, res) => {
+    res.send('Director\'s Bio Selection' );
+  });
+
+  //allow new user to register
+app.post('/users', (req, res) => {
+    res.send('New User added'); 
+});
+
+//allow user to add movie to their list of favourites
+app.put('/favourites/add/:title', (req, res) => {
+    res.send('Added Movie');
+  })
+  
+  //allow user to remove a movie from list of favourites
+  app.delete('/favourites/remove/:title', (req, res) => {
+    res.send('Remove Movie');
+  });
+  
+  //allow existing user to deregister
+  app.delete('/users/delete/:name', (req, res) => {
+    res.send('Deleted User');
+  });
+
+
+  
 app.get('/', (req, res) =>{
     res.send('Welcome to my App')
 });
