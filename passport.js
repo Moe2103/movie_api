bbedacd94c1778edef0da36767549622da346559
Moe2103 <95778781+Moe2103@users.noreply.github.com>
2022-03-28 18:24:30 +0200

    2.10

--- passport.js
+++ passport.js
@@ -2,11 +2,9 @@ const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
   passportJWT = require('passport-jwt');
-
 let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
-
 passport.use(new LocalStrategy({
   usernameField: 'Username',
   passwordField: 'Password'
@@ -17,17 +15,20 @@ passport.use(new LocalStrategy({
       console.log(error);
       return callback(error);
     }
-
     if (!user) {
       console.log('incorrect username');
       return callback(null, false, {message: 'Incorrect username or password.'});
+    }
+
+    if(!user.validatePassword(password)) {
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect password.'}); 
     }
 
     console.log('finished');
     return callback(null, user);
   });
 }));
-
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'
